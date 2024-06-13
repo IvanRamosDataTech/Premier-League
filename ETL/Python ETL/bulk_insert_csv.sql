6e7875991f37fca9d360209b68272da3924dfd16
IvanRamosDataTech <ajaxiank@gmail.com>
2024-06-13 12:48:43 -0600

    Adding bulk copy script to import csv into match history table in postgres

---
+++ ETL/Python ETL/bulk_insert_csv.sql
@@ -0,0 +1,9 @@
+-- Don't forget to first copy your csv data source into container's file system so Postgresql can find it
+-- and proceed with bulk inserts. You can perform this by typing following command:
+
+--docker cp dataset/PremierLeague.csv container-pg:/home/PremierLeague.csv
+
+COPY match_history
+FROM '/home/PremierLeague.csv'
+DELIMITER ','
+CSV HEADER;
