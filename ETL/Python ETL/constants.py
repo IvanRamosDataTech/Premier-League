c0e18c2fcd679b424b94b470581480ccbed95761
IvanRamosDataTech <ajaxiank@gmail.com>
2024-07-02 18:02:52 -0600

    - Fix weird character in EXCEL Maual ETL for matchweek 36 , season 2004-2005 - Remove test dataset - Define path for master csv

--- ETL/Python ETL/constants.py
+++ ETL/Python ETL/constants.py
@@ -3,11 +3,9 @@
 """
 
 CSV_SOURCE_URL = 'https://www.football-data.co.uk/mmz4281/2324/E0.csv'
-## !!! TEMP variable, let's use a local csv for testing purposes
-# CSV_SOURCE_URL = "../../dataset/PremierLeagueTESTDATA.csv"
-
 CURRENT_SEASON_TAG = "2023-2024"
 DEFAULT_MATCHWEEK = 1
+MASTER_CSV_URL = "../../dataset/PremierLeague.csv"
 
 # Database constants
 DB_SERVER = "localhost"
