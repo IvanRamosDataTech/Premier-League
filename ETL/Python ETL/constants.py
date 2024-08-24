6e387af1ea27e20a6c7b95b85d07acd526ee21ff
IvanRamosDataTech <ajaxiank@gmail.com>
2024-08-23 19:50:04 -0600

    - update CSV_SOURCE_URL, CURRENT_SEASON_TAG variables

--- ETL/Python ETL/constants.py
+++ ETL/Python ETL/constants.py
@@ -2,8 +2,8 @@
     This module defines project-level constants
 """
 
-CSV_SOURCE_URL = 'https://www.football-data.co.uk/mmz4281/2324/E0.csv'
-CURRENT_SEASON_TAG = "2023-2024"
+CSV_SOURCE_URL = 'https://www.football-data.co.uk/mmz4281/2425/E0.csv'
+CURRENT_SEASON_TAG = "2024-2025"
 DEFAULT_MATCHWEEK = 1
 
 # Database constants
